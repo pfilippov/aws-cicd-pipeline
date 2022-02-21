7c2f844d2e209b07a965d2847bb70ddf0ffd0dde
pavelfilippov <p.g.filippov@gmail.com>
2022-02-21 18:15:53 +1300

    Adding the aws_s3_bucket_acl resource

--- modules/cicd/main.tf
+++ modules/cicd/main.tf
@@ -217,5 +217,13 @@ resource "aws_codepipeline" "cicd_pipeline" {
 
 resource "aws_s3_bucket" "codepipeline_artifacts" {
   bucket = var.cicd_artefacts_bucket_name
+
+  tags = merge({
+    env = var.environment
+  }, var.tags)
+}
+
+resource "aws_s3_bucket_acl" "codepipeline_artifacts" {
+  bucket = aws_s3_bucket.codepipeline_artifacts.id
   acl    = "private"
 }
