b2f85c3ecd15cc909a233c4bba5efa95a885851c
pavelfilippov <p.g.filippov@gmail.com>
2022-03-07 17:00:21 +1300

    Updated CIDR

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -83,5 +83,5 @@ variable "cicd_approval_message" {
 variable "vpc_cidr_block" {
   description = "VPC CIDR Block"
   type        = string
-  default     = "1.2.0.0/24"
+  default     = "1.2.0.0/28"
 }
